40e27b056498bc6a55f66d41991f1797b6daab52
Craig Phillips <iwonbigbro@gmail.com>
2013-04-27 02:27:42 +0100

    Removed threading model and implemented multiprocessing model instead. Implemented better directory traversal so behaviour is identical to rsync. Added timestamp checking on source and destination files to ensure updating works as intended.  Notes: Folders are not updated when in update mode even when --times is specified.  If --times is exclusively used without --update, then timestamps are updated on the destination if they don't match the source, otherwise only files are updated.

--- libgsync/crawler.py
+++ libgsync/crawler.py
@@ -1,32 +1,38 @@
 # Copyright (C) 2013 Craig Phillips.  All rights reserved.
 
-import os, re
-from threading import Thread
+import os, re, traceback, sys
+#from threading import Thread
+from multiprocessing import Process
+from libgsync.sync import Sync
 from libgsync.drive import Drive, MimeTypes
 from libgsync.output import verbose, debug
 from libgsync.options import Options
 from libgsync.bind import bind
 
-class Crawler(Options, Thread):
+class Crawler(Options, Process):
     _dev = None
     _drive = None
+    _src = None
+    _dst = None
+    _options = None
 
-    def __init__(self, path, callback, options):
-        self._callback = callback
+    def __init__(self, src, dst, options):
+        self._options = options
+        self._dst = dst
 
         self.initialiseOptions(options)
 
-        if re.search(r'^drive://', path) is None:
-            self._path = path
-            st_info = os.stat(self._path)
+        if re.search(r'^drive://', src) is None:
+            self._src = src
+            st_info = os.stat(self._src)
 
             if self._opt_one_file_system:
                 self._dev = st_info.st_dev
         else:
             self._drive = Drive()
-            self._path = re.sub(r'^drive://+', "/", path)
+            self._src = re.sub(r'^drive://+', "/", src)
 
-        Thread.__init__(self, name = "Crawler: %s" % path)
+        super(Crawler, self).__init__(name = "Crawler: %s" % src)
     
 
     def _devCheck(self, dev, path):
@@ -41,39 +47,48 @@ class Crawler(Options, Thread):
 
     def _walk(self, path, generator, dev):
         for d, dirs, files in generator(path):
-            if self._opt_dirs:
+            debug("Walking: %s" % d)
+
+            if not self._devCheck(dev, d):
+                debug("Not on same device: %s" % d)
+                continue
+
+            if self._opt_dirs or self._opt_recursive:
                 # Sync the directory but not its contents
+                debug("Synchronising directory: %s" % d)
                 self._callback(d)
+            else:
+                sys.stdout.write("skipping directory %s\n" % d)
+                break
 
             for f in files:
                 f = os.path.join(d, f)
                 if not self._devCheck(dev, f):
                     continue
                     
+                debug("Synchronising file: %s" % f)
                 self._callback(f)
-
-            # Do nothing with directories if we are not synchronising them
-            if self._opt_dirs:
-                for subd in dirs:
-                    subd = os.path.join(d, subd)
-
-                    # Sync the directory but not its contents
-                    self._callback(subd)
-
-                    if self._opt_recursive and not self._devCheck(dev, subd):
-                        continue
 
             if not self._opt_recursive:
                 break
 
 
     def run(self):
-        path = self._path
+        srcpath = self._src
         generator = None
 
+        (basepath, path) = os.path.split(srcpath)
+
+        if self._opt_relative:
+            # Supports the foo/./bar notation in rsync.
+            path = re.sub(r'^.*/\./', "", path)
+
+        self._callback = Sync(basepath, self._dst, self._options)
+
         if self._drive is None:
-            debug("Enumerating: %s" % path)
-            self._walk(path, os.walk, self._dev)
+            debug("Enumerating: %s" % srcpath)
+            self._walk(srcpath, os.walk, self._dev)
         else:
-            debug("Enumerating: drive://%s" % path)
-            self._walk(path, bind("walk", self._drive), None)
+            debug("Enumerating: drive://%s" % srcpath)
+            self._walk(srcpath, bind("walk", self._drive), None)
+
--- libgsync/sync.py
+++ libgsync/sync.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2013 Craig Phillips.  All rights reserved.
 
-import os, time, re
+import os, datetime, time, re
 from libgsync.output import verbose, debug, itemize
 from libgsync.drive import Drive, MimeTypes
 from libgsync.options import Options
@@ -20,12 +20,32 @@ class SyncFileInfo(object):
         self.id = id
         self.title = title
         self.modifiedDate = modifiedDate
+        self.__modifiedDate = datetime.datetime.strptime(
+            modifiedDate,
+            "%a %b %d %H:%M:%S %Y"
+        )
         self.mimeType = mimeType
 
-    
-class SyncFile(object):
-    path = None
+    def __gt__(self, fileInfo):
+        return bool(self.__modifiedDate > fileInfo.__modifiedDate)
 
+    def __lt__(self, fileInfo):
+        return bool(self.__modifiedDate < fileInfo.__modifiedDate)
+
+    def __ge__(self, fileInfo):
+        return bool(self.__modifiedDate >= fileInfo.__modifiedDate)
+
+    def __le__(self, fileInfo):
+        return bool(self.__modifiedDate <= fileInfo.__modifiedDate)
+
+    def __eq__(self, fileInfo):
+        return bool(self.__modifiedDate == fileInfo.__modifiedDate)
+
+    def __ne__(self, fileInfo):
+        return bool(self.__modifiedDate != fileInfo.__modifiedDate)
+
+
+class SyncFileFactory(object):
     @staticmethod
     def create(path):
         path = re.sub(r'/+$', "", path)
@@ -38,6 +58,10 @@ class SyncFile(object):
         debug("Creating SyncFileRemote(%s)" % filepath)
         return SyncFileRemote(filepath)
 
+    
+class SyncFile(object):
+    path = None
+
     def __init__(self, path):
         self.path = path
 
@@ -45,30 +69,66 @@ class SyncFile(object):
         return self.path
 
     def __add__(self, path):
-        return self.path + path
+        return os.path.join(self.path, path)
 
-    def getInfo(self, path):
+    def getInfo(self, path = None):
         raise ESyncFileAbstractMethod
 
-    def createDir(self, path):
+    def _createDir(self, path):
         raise ESyncFileAbstractMethod
 
-    def openFile(self, path):
+    def _createFile(self, path, src):
         raise ESyncFileAbstractMethod
+
+    def _updateFile(self, path, src):
+        raise ESyncFileAbstractMethod
+
+    def _normaliseSource(self, src):
+        srcInfo = None
+
+        if src is not None:
+            if not isinstance(src, SyncFile):
+                src = SyncFileFactory.create(src)
+
+            srcInfo = src.getInfo()
+
+        return (src, srcInfo)
+
+    def create(self, path, src = None):
+        (src, srcInfo) = self._normaliseSource(src)
+
+        if srcInfo is None or srcInfo.mimeType == MimeTypes.FOLDER:
+            self._createDir(path)
+            return
+
+        self._createFile(path, src)
+
+    def update(self, path, src):
+        (src, srcInfo) = self._normaliseSource(src)
+
+        if srcInfo is None or srcInfo.mimeType == MimeTypes.FOLDER:
+            return
+
+        self._updateFile(path, src)
 
 
 class SyncFileLocal(SyncFile):
+    def getInfo(self, path = None):
+        if path is None:
+            path = self.path
+        else:
+            debug("Joining: %s with %s" % (self.path, path))
+            path = os.path.join(self.path, path)
+            debug("Got: %s" % path)
 
-    def getInfo(self, path):
         debug("Fetching local file metadata: %s" % path)
 
         try:
             # Obtain the file info, following the link
-            realpath = os.path.realpath(path)
-            st_info = os.stat(realpath)
+            st_info = os.stat(path)
             dirname, filename = os.path.split(path)
 
-            if os.path.isdir(realpath):
+            if os.path.isdir(path):
                 mimeType = MimeTypes.FOLDER
             else:
                 mimeType = MimeTypes.NONE
@@ -87,23 +147,24 @@ class SyncFileLocal(SyncFile):
 
         return info
 
-    def createDir(self, path):
+    def _createDir(self, path):
         debug("Creating local directory: %s" % path)
-        pass
 
-    def openFile(self, path, create = False):
-        if create:
-            debug("Creating local file: %s" % path)
-            return None
+    def _createFile(self, path, src):
+        debug("Creating local file: %s" % path)
 
-        debug("Opening local file: %s" % path)
-        return None
+    def _updateFile(self, path, src):
+        debug("Updating local file: %s" % path)
 
 
 class SyncFileRemote(SyncFile):
-
-    def getInfo(self, path):
+    def getInfo(self, path = None):
         global g_drive
+
+        if path is None:
+            path = self.path
+        else:
+            path = os.path.join(self.path, path)
 
         debug("Fetching remote file metadata: %s" % path)
 
@@ -117,17 +178,14 @@ class SyncFileRemote(SyncFile):
 
         return info
 
-    def createDir(self, path):
+    def _createDir(self, path):
         debug("Creating remote directory: %s" % path)
-        pass
 
-    def openFile(self, path, create = False):
-        if create:
-            debug("Creating remote file: %s" % path)
-            return None
+    def _createFile(self, path, src):
+        debug("Creating remote file: %s" % path)
 
-        debug("Opening remote file: %s" % path)
-        return None
+    def _updateFile(self, path, src):
+        debug("Updating remote file: %s" % path)
 
 
 class Sync(Options):
@@ -139,59 +197,73 @@ class Sync(Options):
         g_drive = Drive()
         self.initialiseOptions(options)
 
-        self.src = SyncFile.create(src)
-        self.dst = SyncFile.create(dst)
+        self.src = SyncFileFactory.create(src)
+        self.dst = SyncFileFactory.create(dst)
 
     def __call__(self, path):
-        changes, dst = self._sync(path)
-        if self._opt_itemize_changes:
-            itemize(changes, dst)
+        itemize_args = self._sync(path)
+
+        if itemize_args is not None and self._opt_itemize_changes:
+            itemize(*itemize_args)
 
     def _sync(self, path):
         debug("Synchronising: %s" % path)
 
-        if self._opt_relative:
-            # Supports the foo/./bar notation in rsync.
-            dstPath = self.dst + re.sub(r'^.*/\./', "", path)
-        else:
-            dstPath = self.dst + path[len(str(self.src)):]
+        relPath = re.sub(r'^%s/' % str(self.src), "", path)
+        debug("Destination: %s" % self.dst)
+        debug("Relative: %s" % relPath)
 
-        srcFile = self.src.getInfo(path)
+        srcFile = self.src.getInfo(relPath)
+        dstPath = self.dst + relPath
         dstFile = self.dst.getInfo(dstPath)
+        create = False
+        update = False
+        folder = bool(srcFile.mimeType == MimeTypes.FOLDER)
 
-        if dstFile is None:
+        if dstFile is None or dstFile.mimeType != srcFile.mimeType:
             changes = bytearray(">++++++++++")
+            create = True
         else:
             changes = bytearray("...........")
 
-        if srcFile.mimeType == MimeTypes.FOLDER:
-            changes[0] = 'c'
+        if self._opt_times:
+            if (not self._opt_update or folder) and srcFile != dstFile:
+                changes[4] = 't'
+                update = True
+            else:
+                debug("File up to date: %s" % path)
+                return None
+        elif not self._opt_update or srcFile > dstFile:
+            if folder:
+                debug("Don't update folders unless --times: %s" % path)
+                return None
+
+            changes[4] = 'T'
+            update = True
+        else:
+            debug("File up to date: %s" % path)
+            return None
+
+        if folder:
+            if create:
+                changes[0] = 'c'
+
             changes[1] = 'd'
+            relPath += "/"
         else:
             changes[1] = 'f'
 
-        if dstFile is not None and self._opt_ignore_existing:
+            if update or create:
+                changes[0] = '>'
+
+        if create:
+            self.dst.create(dstPath, path)
+
+        elif self._opt_ignore_existing:
             debug("File exists on the receiver, skipping: %s" % path)
-            return (changes, dstPath)
+            return None
 
-        # A directory will have a trailing /.  Create directories and return.
-        if srcFile.mimeType == MimeTypes.FOLDER:
-            if dstFile is None:
-                self.dst.createDir(dstPath)
-            return (changes, dstPath)
-
-        # TODO: This needs some work.  When opening a file, we should check
-        # modified date / checksum to see what file is newer and if they
-        # differ and need synchronising.
-
-        # Create file if it doesn't exist.
-        if dstFile is None:
-            create = True
         else:
-            create = False
+            self.dst.update(dstPath, path)
 
-        f = self.dst.openFile(dstPath, create = create)
-
-        # TODO: Synchronise the file contents.
-
-        return (changes, dstPath)
+        return (changes, relPath)
