3e791db326635b3fc235fa12ebf8b401661d29b3
Craig Phillips <iwonbigbro@gmail.com>
2013-09-30 22:54:52 +0100

    New documentation image and missed checkin of client_json.py.

---
+++ libgsync/drive/client_json.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2013 Craig Phillips.  All rights reserved.
+
+client_obj = {
+    "installed": {
+        "client_id": "542942405111.apps.googleusercontent.com",
+        "client_secret": "Y4iSAluo7pCY57m8HFOfv2W_",
+        "redirect_uris": [
+            "http://localhost", "urn:ietf:oauth:2.0:oob"
+            ],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token"
+    }
+}
