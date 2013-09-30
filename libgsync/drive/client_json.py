236bb25546cdfbf162087a82aa94d90e070a6c84
Craig Phillips <iwonbigbro@gmail.com>
2013-09-30 23:06:07 +0100

    Version 0.1.2

--- libgsync/drive/client_json.py
+++ libgsync/drive/client_json.py
@@ -6,7 +6,7 @@ client_obj = {
         "client_secret": "Y4iSAluo7pCY57m8HFOfv2W_",
         "redirect_uris": [
             "http://localhost", "urn:ietf:oauth:2.0:oob"
-            ],
+        ],
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://accounts.google.com/o/oauth2/token"
     }
