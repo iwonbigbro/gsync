4ebdc924d98a57c4d84397ab7c454cbfadec96f0
Craig Phillips <iwonbigbro@gmail.com>
2014-04-02 23:13:42 +0100

    Fix up unittests for gsync options.

--- tests/libgsync/test_options.py
+++ tests/libgsync/test_options.py
@@ -13,36 +13,28 @@ class TestGsyncOptions(unittest.TestCase):
     def tearDown(self):
         sys.argv = self.argv
 
-    def test_00_is_the_only_object_in_the_module(self):
-        import libgsync.options
-        found = []
-        expected = [ "GsyncOptions" ]
-
-        for k in libgsync.options.__dict__.iterkeys():
-            if k.startswith("__"): continue
-            found.append(k)
-
-        self.assertEqual(found, expected)
-
     def test_01_is_initialised_on_property_inspection(self):
         import libgsync.options
         GsyncOptions = libgsync.options.GsyncOptions
         GsyncListOptions = GsyncOptions.list()
-        GsyncListOptionsType = object.__getattribute__(
-            GsyncListOptions, "__metaclass__"
+        GsyncListOptionsType = libgsync.options.GsyncListOptionsType
+
+        def hooked_init(func):
+            def __hooked_init(*args, **kwargs):
+                hooked_init.call_count += 1
+                func(*args, **kwargs)
+
+            return __hooked_init
+
+        hooked_init.call_count = 0
+
+        GsyncListOptionsType._GsyncListOptionsType__initialise_class = hooked_init(
+            GsyncListOptionsType._GsyncListOptionsType__initialise_class
         )
 
-        init = GsyncListOptionsType._GsyncListOptionsType__initialiseClass
-        def hookedInit(*args, **kwargs):
-            hookedInit.call_count += 1
-            init(*args, **kwargs)
-
-        hookedInit.call_count = 0
-        GsyncListOptionsType._GsyncListOptionsType__initialiseClass = hookedInit
-
         self.assertNotEqual(GsyncOptions.debug, None)
         self.assertNotEqual(GsyncOptions.debug, None)
-        self.assertEqual(1, hookedInit.call_count)
+        self.assertEqual(1, hooked_init.call_count)
 
     def test_02_list_options(self):
         import libgsync.options
